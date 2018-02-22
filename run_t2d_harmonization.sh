cb42b2d2a72452add4e6ed3da7257c16b262b630
Timothy Majarian <tmajaria@broadinstitute.org>
2018-02-22 11:22:13 -0500

    shell script changes

--- run_t2d_harmonization.sh
+++ run_t2d_harmonization.sh
@@ -2,10 +2,38 @@
 # $1 = f.dir
 # $2 = source.file
 # $3 = out.pref
+# $4 = trait 
+
+spinner()
+{
+    local pid=$1
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+echo "phenotype file directory : $1"
+echo "filepath script path : $2"
+echo "output file prefix : $3"
+echo "Desired trait to removed duplicates : $4"
+echo "Output harmonized file : $3.csv"
+echo "\n"
 
 # make the pooled trait file
-R --vanilla --args $1 $2 $3 < Harmonization.19JAN2017.GitHub.R
+(R --vanilla --args $1 $2 $3 < Harmonization.19JAN2017.GitHub.R > /dev/null 2>&1) &
+echo "Running harmonization" 
+spinner $! 
+echo "Done!"
+echo "\n"
 
-# removed the duplicates
-
-Rscript duplicates.R
+(R --vanilla --args $1 topmedid $3.csv $4 $3 < duplicates_v2_TM_022218.R > /dev/null 2>&1) &
+echo "Running duplicates script" 
+spinner $! 
+echo "Done!"
