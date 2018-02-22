c8e109c211b595aea87846f509c4b0af7bfde537
Timothy Majarian <tmajaria@broadinstitute.org>
2018-02-22 12:28:31 -0500

    no longer need to input filepath script location, assumes that you run from the git dir

--- run_t2d_harmonization.sh
+++ run_t2d_harmonization.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # $1 = f.dir
-# $2 = source.file
-# $3 = out.pref
-# $4 = trait 
+# $2 = out.pref
+# $3 = trait 
 
 # get a unique identified for the users current branch, this links back to the last commit on the branch that the user is working. Can be used to see the exact code state of the code (to the last remote commit)
 git_tag=$(git rev-list HEAD | head -1)
@@ -25,14 +24,13 @@ spinner()
 }
 
 echo "phenotype file directory : $1"
-echo "filepath script path : $2"
-echo "output file prefix : $3"
-echo "Desired trait to removed duplicates : $4"
+echo "output file prefix : $2.$user.$curdate"
+echo "Desired trait to removed duplicates : $3"
 echo "\n"
 
 echo "harmonization:\n" > $1/harm.stdout
 # make the pooled trait file
-(R --vanilla --args $1 $2 $3.$user.$curdate < Harmonization.19JAN2017.GitHub.R >> $1/harm.stdout 2>&1) &
+(R --vanilla --args $1 $2.$user.$curdate < Harmonization.19JAN2017.GitHub.R >> $1/harm.stdout 2>&1) &
 echo "Running harmonization" 
 spinner $! 
 echo "Done!"
@@ -40,14 +38,14 @@ echo "\n"
 
 echo "\nduplicates:\n" >> $1/harm.stdout
 
-(R --vanilla --args $1 $3.$user.$curdate topmedid $3.$user.$curdate.csv $4 < duplicates_v2_TM_022218.R >> $1/harm.stdout 2>&1) &
+(R --vanilla --args $1 $2.$user.$curdate topmedid $2.$user.$curdate.csv $3 < duplicates_v2_TM_022218.R >> $1/harm.stdout 2>&1) &
 echo "Running duplicates script" 
 spinner $! 
 echo "Done!"
 
-echo "git commit tag of current code : $git_tag" > $1/$3.$user.$curdate.log
-echo "harmonized phenotypes with duplicates : $1/$3.$user.$curdate.csv" >> $1/$3.$user.$curdate.log
-echo "harmonized phenotypes without duplicates : $1/$3.$user.$curdate.no.duplicates.csv" >> $1/$3.$user.$curdate.log
-echo "list of NWD ids that were removed : $1/$3.$user.$curdate.removed.IDs.txt" >> $1/$3.$user.$curdate.log
-echo "duplicates info table : $1/$3.$user.$curdate.duplicates.txt" >> $1/$3.$user.$curdate.log
-echo "stdout and stderr file : $1/harm.stdout" >> $1/$3.$user.$curdate.log
+echo "git commit tag of current code : $git_tag" > $1/$2.$user.$curdate.log
+echo "harmonized phenotypes with duplicates : $1/$2.$user.$curdate.csv" >> $1/$2.$user.$curdate.log
+echo "harmonized phenotypes without duplicates : $1/$2.$user.$curdate.no.duplicates.csv" >> $1/$2.$user.$curdate.log
+echo "list of NWD ids that were removed : $1/$2.$user.$curdate.removed.IDs.txt" >> $1/$2.$user.$curdate.log
+echo "duplicates info table : $1/$2.$user.$curdate.duplicates.txt" >> $1/$2.$user.$curdate.log
+echo "stdout and stderr file : $1/harm.stdout" >> $1/$2.$user.$curdate.log
