eaab5dafa9de8023ba861d3d479286a4c8dfe8d6
Timothy Majarian <tmajaria@broadinstitute.org>
2018-02-26 14:26:31 -0500

    shell to run gly harm

---
+++ run_gly_harmonization.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# $1 = f.dir
+# $2 = out.pref
+# $3 = trait 
+
+# get a unique identified for the users current branch, this links back to the last commit on the branch that the user is working. Can be used to see the exact code state of the code (to the last remote commit)
+git_tag=$(git rev-list HEAD | head -1)
+user=$(whoami)
+curdate=$(date +"%m.%d.%Y")
+
+spinner()
+{
+    local pid=$1
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+echo "phenotype file directory : $1"
+echo "output file prefix : $2.$user.$curdate"
+echo "Desired trait to removed duplicates : $3"
+echo "\n"
+
+echo "harmonization:\n" > $1/harm.stdout
+# make the pooled trait file
+(R --vanilla --args $1 $2.$user.$curdate < gly_traits_harmonization_021318.R >> $1/harm.stdout 2>&1) &
+echo "Running harmonization" 
+spinner $! 
+echo "Done!"
+echo "\n"
+
+echo "\nduplicates:\n" >> $1/harm.stdout
+
+(R --vanilla --args $1 $2.$user.$curdate topmedid $2.$user.$curdate.csv $3 < duplicates_v2_TM_022218.R >> $1/harm.stdout 2>&1) &
+echo "Running duplicates script" 
+spinner $! 
+echo "Done!"
+
+echo "git commit tag of current code : $git_tag" > $1/$2.$user.$curdate.log
+echo "harmonized phenotypes with duplicates : $1/$2.$user.$curdate.csv" >> $1/$2.$user.$curdate.log
+echo "harmonized phenotypes without duplicates : $1/$2.$user.$curdate.no.duplicates.csv" >> $1/$2.$user.$curdate.log
+echo "list of NWD ids that were removed : $1/$2.$user.$curdate.removed.IDs.txt" >> $1/$2.$user.$curdate.log
+echo "duplicates info table : $1/$2.$user.$curdate.duplicates.txt" >> $1/$2.$user.$curdate.log
+echo "stdout and stderr file : $1/harm.stdout" >> $1/$2.$user.$curdate.log
