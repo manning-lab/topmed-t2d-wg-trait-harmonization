eeac7b101abb1c5dcb2ac335f55ec0a020c5b611
Timothy Majarian <tmajaria@broadinstitute.org>
2018-03-15 16:03:30 -0400

    m:

--- duplicates_v2_TM_022218.R
+++ duplicates_v2_TM_022218.R
@@ -169,7 +169,7 @@ if(length(args) < 1) {
     ### for reordering ID1 and ID2. Used column names for clarity but can use column numbers for simplicity. 
     
     
-    write.table(dups,paste(out.pref,"duplicates.txt",sep=""),row.names=F,col.names=T,quote=F,sep='\t')
+    write.table(dups,paste(f.dir,"/",out.pref,"duplicates.txt",sep=""),row.names=F,col.names=T,quote=F,sep='\t')
     
     
   # }
@@ -323,7 +323,7 @@ if(length(args) == 5) {
                  'JHS','MESA','MGH_AF','Partners','SAFS','SAS','VAFAR','VU_AF','WHI'))
 
     ped <- ped[!is.na(ped[,trait]),]
-    write.table(ped[ped$keep_trait == 1,],paste(f.dir,"/","no.duplicates.csv",sep=""),row.names=F,col.names=T,quote=F,sep=',')
+    write.table(ped[ped$keep_trait == 1,],paste(f.dir,"/",out.pref,"no.duplicates.csv",sep=""),row.names=F,col.names=T,quote=F,sep=',')
     
     ## NOTE: 13MAR2018 sex.x and sex.y from fulldata (harmonization) file is dropped
     
@@ -350,7 +350,7 @@ if(length(args) == 5) {
     
     removed_TRAIT <- rbind(removed1_TRAIT, removed2_TRAIT)
     
-    write.table(removed_TRAIT, paste(f.dir,"/","removed.IDs.txt",sep=""), row.names=F, col.names=T, quote=F, sep=',')
+    write.table(removed_TRAIT, paste(f.dir,"/",out.pref,"removed.IDs.txt",sep=""), row.names=F, col.names=T, quote=F, sep=',')
     }
 }
 
